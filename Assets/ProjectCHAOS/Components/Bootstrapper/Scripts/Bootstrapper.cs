db88be1205bb6f9ca425693bfbb4dddce3b0beca
Admor Aguilar <admor.aguilar@gmail.com>
2021-12-30 10:56:49 +0100

    + Create Bootstrapper system

--- Assets/ProjectCHAOS/Components/Bootstrapper/Scripts/Bootstrapper.cs
+++ Assets/ProjectCHAOS/Components/Bootstrapper/Scripts/Bootstrapper.cs
@@ -4,35 +4,18 @@ using UnityEngine.SceneManagement;
 namespace ProjectCHAOS.Bootup
 {
     [CreateAssetMenu(menuName = "ProjectCHAOS/Bootstrapper")]
-    public class Bootstrapper : ScriptableObject
+    public class Bootstrapper : ScriptableSingleton<Bootstrapper>
     {
-		private static Bootstrapper _instance = null;
-
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void RunOnLoad()
 		{
-			if(!_instance.isEnabled) { return; }
-			SceneManager.LoadSceneAsync(_instance.sceneName, LoadSceneMode.Additive);
+			if(!Instance.isEnabled) { return; }
+			SceneManager.LoadSceneAsync(Instance.sceneName, LoadSceneMode.Additive);
 		}
+
+
 
 		public bool isEnabled = false;
         public string sceneName = string.Empty;
-
-		private void OnEnable()
-		{
-			if(_instance != null) {
-				Destroy(this);
-				return;
-			}
-
-			_instance = this;
-		}
-
-		private void OnDisable()
-		{
-			if(_instance == this) {
-				_instance = null;
-			}
-		}
 	}
 }
