8dfe3da9d220b637837932073fe30febca0be572
Admor Aguilar <admor.aguilar@gmail.com>
2021-12-30 10:56:26 +0100

    + Create Singleton utilities

---
+++ Assets/ProjectCHAOS/Components/Utilities/Singleton/ScriptableSingleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProjectCHAOS
+{
+	public class ScriptableSingleton<T> : ScriptableObject where T : ScriptableObject
+	{
+		public static T Instance
+		{
+			get {
+				if(_instance == null) {
+					throw new NullReferenceException($"No scriptable object instance found. Make sure you have one initialized for [{nameof(T)}] on Player Settings -> Other Settings -> Preloaded Assets.");
+				}
+				return _instance;
+			}
+		}
+		private static T _instance = default;
+
+		private void OnEnable()
+		{
+			if(_instance != null && _instance != this) {
+				DestroyImmediate(this, true);
+				return;
+			}
+
+			_instance = this as T;
+		}
+
+		private void OnDisable()
+		{
+			if(_instance == this) {
+				_instance = null;
+			}
+		}
+	}
+}
