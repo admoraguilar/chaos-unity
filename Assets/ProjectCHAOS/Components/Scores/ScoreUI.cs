16f18b3018ef699d2b60f9d9f9891ea99e54d795
Admor Aguilar <admor.aguilar@gmail.com>
2022-02-05 22:17:59 +0100

    ~ Implement best score on ScoreUI

--- Assets/ProjectCHAOS/Components/Scores/ScoreUI.cs
+++ Assets/ProjectCHAOS/Components/Scores/ScoreUI.cs
@@ -6,13 +6,23 @@ namespace ProjectCHAOS.Scores
 	public class ScoreUI : MonoBehaviour
 	{
 		[SerializeField]
-		private TMP_Text _text = null;
+		private TMP_Text _currentText = null;
+
+		[SerializeField]
+		private TMP_Text _bestText = null;
 
 		private Score _score = null;
 
 		private void OnScoreUpdate(int score)
 		{
-			_text.text = score.ToString();
+			if(_currentText == null) { return; }
+			_currentText.text = score.ToString();
+		}
+
+		private void OnScoreNewBest(int newBestScore)
+		{
+			if(_bestText == null) { return; }
+			_bestText.text = newBestScore.ToString();
 		}
 
 		private void Awake()
@@ -20,19 +30,22 @@ namespace ProjectCHAOS.Scores
 			_score = Scorer.Instance.GetScore(0);
 		}
 
-		private void Start()
-		{
-			_text.text = "0";
-		}
-
 		private void OnEnable()
 		{
-			_score.OnUpdate += OnScoreUpdate;
+			if(_currentText != null) { 
+				_score.OnUpdate += OnScoreUpdate;
+				_currentText.text = _score.current.ToString();
+			}
+			if(_bestText != null) { 
+				_score.OnNewBest += OnScoreNewBest;
+				_bestText.text = _score.best.ToString();
+			}
 		}
 
 		private void OnDisable()
 		{
-			_score.OnUpdate -= OnScoreUpdate;
+			if(_currentText != null) { _score.OnUpdate -= OnScoreUpdate; }
+			if(_bestText != null) { _score.OnNewBest -= OnScoreNewBest; }
 		}
 	}
 }
